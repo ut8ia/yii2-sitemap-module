a8f89863e716390afc7e7f32f3e59eb14b3690bf
Eugene Anufriev <eugene.anufriev@gmail.com>
2017-08-02 17:58:25 +0300

    initial commit

---
+++ SitemapModule.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace ut8ia\sitemapmodule;
+
+use \yii\base\Module;
+
+/**
+ * Class SitemapModule
+ * @package ut8ia\contentmodule
+ */
+class SitemapModule extends Module
+{
+
+    public function init()
+    {
+        parent::init();
+    }
+
+
+}
+
+
+?>
---
+++ controllers/SitemapController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace ut8ia\sitemapmodule\controllers;
+
+use yii\web\Controller;
+
+class SitemapController extends Controller
+{
+
+    /**
+     * @return string
+     */
+    public function actionIndex()
+    {
+        return "sitemap here";
+    }
+
+
+}
